64d12040b7c3eec8e928b211ae7415a66ae834c7
Rokas Lengvenis <rokas.lengvenis@gmail.com>
2025-11-08 12:42:04 +0000

    fix: broken HMR feature

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,11 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100,
+    },
+    hmr: {
+      overlay: true,
+    },
+  },
 })
